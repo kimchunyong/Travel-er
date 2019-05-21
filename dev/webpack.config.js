0cad1ee0081b59249e21f17f7139e8c5e9123ece
kimchunyong <lovemy5854@naver.com>
2019-05-21 22:56:33 +0900

    [complete]  환경설정 완료

--- dev/webpack.config.js
+++ dev/webpack.config.js
@@ -4,49 +4,48 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const port = process.env.PORT || 8080;
 
 module.exports = {
-    //개발모드 production이면 배포
-    mode:'development',
-    entry:'./src/index.js',
-    devtool: 'inline-source-map',
-    devServer: {
-        host: 'localhost',
-        port: port,
-        open: true,
-        historyApiFallback: true
-    },
-    output:{
-        path: __dirname + 'dist',
-        filename: 'bundle.[hash].js'
-    },
-    module:{
-        rules:[
-            { // 첫번째 룰
-                test:\.(js)$/,
-                exclude:/node_modules/,
-                use:['babel-loader']
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.[hash].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              camelCase: true,
+              sourceMap: true,
             },
-            { // 두번째 룰
-                test: /\.css$/,
-                use: [
-                  {
-                    loader: 'style-loader'
-                  },
-                  {
-                    loader: 'css-loader',
-                    options: {
-                      modules: true,
-                      camelCase: true,
-                      sourceMap: true
-                    }
-                  }
-                ]
-              }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-          template: 'public/index.html',
-          favicon: 'public/favicon.ico'
-        })
+          },
+        ],
+      },
     ],
-}
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+      favicon: 'public/favicon.ico',
+    }),
+  ],
+  devServer: {
+    host: 'localhost',
+    port: port,
+    open: true,
+    historyApiFallback: true,
+  },
+};
