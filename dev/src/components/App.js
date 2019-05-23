3e512cfde02a71c272fedc7990393ab588ac18c6
kimchunyong <lovemy58511@gmail.com>
2019-05-23 15:17:39 +0900

    [add] eslint + prettier

--- dev/src/components/App.js
+++ dev/src/components/App.js
@@ -2,7 +2,13 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return <div>hello!</div>;
+    return (
+        <div className="App">
+          <header className="App-header">
+            
+          </header>
+        </div>
+    )
   }
 }
 
